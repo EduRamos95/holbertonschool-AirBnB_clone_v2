c623cb1c26600fe515230cb720792190452089a3
EduRamos95 <eduramos08.30@gmail.com>
2022-12-01 16:36:46 -0500

    feat: adding public method close()

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -65,3 +65,7 @@ class FileStorage:
             del FileStorage.__objects[key]
             self.save()
             return
+
+    def close(self):
+        """Deserialize JSON file to objects"""
+        self.reload()
