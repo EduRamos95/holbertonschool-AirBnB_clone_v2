190b7bbe74527efdd3e2f826d2e7b1030581d428
EduRamos95 <eduramos08.30@gmail.com>
2022-12-01 14:49:54 -0500

    fix: adding module documentation

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
+""" 
+Module 2-c_route
+    ip 0.0.0.0, port 5000
+    Routes:
+    '/': display “Hello HBNB!”
+    '/hbnb': display “HBNB”
+    '/c/<text>': display “C ” 
+        followed by the value of
+        the text variable 
+        (replace underscore _ symbols with a space )
+    use option strict_slashes=False in your route definition
+"""
 from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
